0e05b4ea00a2289ddf7739816992e5847cf93922
S-Marimbi <marimbinjunge@gmail.com>
2024-06-21 13:13:59 +0300

    Completed OOP-Putting-it-all-together lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def _init_(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  # Use the property setter for validation
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
